5a21b4e6a5e6eb06fba63da16af3beef059ca8cf
Matt Wilson <wilsonmatt@susqu.edu>
2022-09-30 12:06:35 -0400

    Wk4 graded

--- code/Ritchey_Week4.R
+++ code/Ritchey_Week4.R
@@ -1,8 +1,11 @@
 # Look at the plot and model results for our Dryad data in the tutorial. Part 1: Without knowing which points represent which groups, 
   # give one explanation for why these data might be difficult to draw spatial inferences about genes.(3 points)
   #Both axises are large and points seem to be thrown into a white void, with nothing like guidelines to gauge how much soace there actually is. The titles also give us no clue what we are looking at.
+#This is an explanation of why the plot is difficult to read, not why the data are difficult to use. The x and y axes also have consistent values are accurately represent relative distance.
   # Part 2: Despite the drawbacks, give the result or interpretation that you feel most confident in (3 points), and EXPLAIN WHY (4 points).
 #Between the two sets, there is a positive corrwlation as the dots seem to be moving upwards. There are also a few outlyers. 
+#What two sets? I'm not sure what correlation you are talking about...and why are you confident in it?
+
 
 # For your scripting assignment we will use the "ge_data" data frame found in the "stability" package.
   # Install the "stability" package, load it into your R environment, and use the data() function to load the "ge_data". (2 points)
@@ -26,7 +29,7 @@ plot(ge_data$Yield ~ ge_data$Gen)
 
 # Test the significance of both models and look at the model summary. (3 points each)
   # Which model is a better fit to explain the yield response, and WHY? (6 points)
-  #Yield with Env seems to be the best model. When plotted linearly, Yield with Gen seems to be very over fitted. (Run codes above)
+  #Yield with Env seems to be the best model. When plotted linearly, Yield with Gen seems to be very over fitted. (Run codes above) #these are not scatter plots (i.e. not linear). How do the r-squared and p values compare?
   # Hint: Does one model seem more likely to be over-fitted?
 
 anova(mod.env)
@@ -34,4 +37,4 @@ anova(mod.geno)
 summary(mod.env)
 summary(mod.geno)
 # Which environment would be your very WORST choice for generating a strong yield response? (2 points)
-#The worse choice ould be the the Genotype yield response as there is no clear linear response. 
+#The worse choice ould be the the Genotype yield response as there is no clear linear response. #Genotype is not one of the environments.
