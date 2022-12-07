f3c50a2ebfe02647ee24d5e24999427a04acbd8d
MaggieRitchey <ritchey@susqu.edu>
2022-12-07 16:45:22 -0500

    final

--- FInalProject.R
+++ FInalProject.R
@@ -138,4 +138,91 @@ F1T_sonJ_heat<-read.table("F1T_sonJ_heat.txt", header=TRUE)
 colnames(F1T_sonJ_heat)<-c("scaffold","starting","strand","CpG","meth","coverage")
 
 #Now, we move onto data analysis! Yippie!
+#First I am going to calculate percent change from the means of control groups vs their treatments
+#Again, lets start with the diet experiment and testes data
+
+mean(F1T_sonA_nutrition$meth)-mean(F1T_sonA_control$meth) #0.05828337
+mean(F1T_sonA_nutrition$coverage)-mean(F1T_sonA_control$coverage) #5.986203
+
+mean(F1T_sonB_nutrition$meth)-mean(F1T_sonB_control$meth)#-0.03560824
+mean(F1T_sonB_nutrition$coverage)-mean(F1T_sonB_control$coverage)#-1.848875
+
+mean(F1T_sonC_nutrition$meth)-mean(F1T_sonC_control$meth)#-0.04581733
+mean(F1T_sonC_nutrition$coverage)-mean(F1T_sonC_control$coverage)#-3.300234
+
+mean(F1T_sonD_nutrition$meth)-mean(F1T_sonD_control$meth)#0.05194563
+mean(F1T_sonD_nutrition$coverage)-mean(F1T_sonD_control$coverage)#2.944099
+
+mean(F1T_sonE_nutrition$meth)-mean(F1T_sonE_control$meth)#-0.04601685
+mean(F1T_sonE_nutrition$coverage)-mean(F1T_sonE_control$coverage)#-4.416322)
+
+#Now we do percent change in liver data
+
+mean(F1L_sonA_nutrition$meth)-mean(F1L_sonA_control$meth)#-0.02372983
+mean(F1L_sonA_nutrition$coverage)-mean(F1L_sonA_control$coverage)#4.901197
+
+mean(F1L_sonB_nutrition$meth)-mean(F1L_sonB_control$meth)#0.0919865
+mean(F1L_sonB_nutrition$coverage)-mean(F1L_sonB_control$coverage)#1.717459
+
+mean(F1L_sonC_nutrition$meth)-mean(F1L_sonC_control$meth)#-0.06345378
+mean(F1L_sonC_nutrition$coverage)-mean(F1L_sonC_control$coverage)#-9.914445
+
+mean(F1L_sonD_nutrition$meth)-mean(F1L_sonD_control$meth)#0.0232132
+mean(F1L_sonD_nutrition$coverage)-mean(F1L_sonD_control$coverage)#2.437183
+
+mean(F1L_sonE_nutrition$meth)-mean(F1L_sonE_control$meth)#0.03028241
+mean(F1L_sonE_nutrition$coverage)-mean(F1L_sonE_control$coverage)#2.509587
+
+
+#Now we move onto the heat data, starting with percent change in testes.
+
+mean(F1T_sonF_heat$meth)-mean(F1T_sonF_controlheat$meth)#0.02258477
+mean(F1T_sonF_heat$coverage)-mean(F1T_sonF_controlheat$coverage)#0.7178485
+
+mean(F1T_sonG_heat$meth)-mean(F1T_sonG_controlheat$meth)#0.05069408
+mean(F1T_sonG_heat$coverage)-mean(F1T_sonG_controlheat$coverage)#3.475688
+
+mean(F1T_sonH_heat$meth)-mean(F1T_sonH_controlheat$meth)#0.03420734
+mean(F1T_sonH_heat$coverage)-mean(F1T_sonH_controlheat$coverage)#1.953985
+
+mean(F1T_sonI_heat$meth)-mean(F1T_sonI_controlheat$meth)#0.08419306
+mean(F1T_sonI_heat$coverage)-mean(F1T_sonI_controlheat$coverage)#4.394692
+
+mean(F1T_sonJ_heat$meth)-mean(F1T_sonJ_controlheat$meth)#-0.01132418
+mean(F1T_sonJ_heat$coverage)-mean(F1T_sonJ_controlheat$coverage)#-0.9733102
+
+#Is this getting repetitive yet? You guessed it, moving onto liver! One point was accidentally a lowercase l so ignore it. 
+
+mean(F1L_sonF_heat$meth)-mean(F1L_sonF_controlheat$meth)#-0.01324765
+mean(F1L_sonF_heat$coverage)-mean(F1L_sonF_controlheat$coverage)#-45.99898
+
+mean(F1L_sonG_heat$meth)-mean(F1l_sonG_controlheat$meth)#-0.002600873
+mean(F1L_sonG_heat$coverage)-mean(F1l_sonG_controlheat$coverage)#12.3464
+
+mean(F1L_sonH_heat$meth)-mean(F1L_sonH_controlheat$meth)#0.002217709
+mean(F1L_sonH_heat$coverage)-mean(F1L_sonH_controlheat$coverage)#31.62327
+
+mean(F1L_sonI_heat$meth)-mean(F1L_sonI_controlheat$meth)#0.06960815
+mean(F1L_sonI_heat$coverage)-mean(F1L_sonI_controlheat$coverage)#116.0032
+
+mean(F1L_sonJ_heat$meth)-mean(F1L_sonJ_controlheat$meth)#-0.003186264
+mean(F1L_sonJ_heat$coverage)-mean(F1L_sonJ_controlheat$coverage)#-12.06475
+
+#So now, this data can be put into excel sheet. Then we use 'readxl' to bring in the data. 
+install.packages("readxl")
+library(readxl)
+setwd("C:/GitHub/ritcheyms")
+#I saved this excel into my GitHUb as it will fit here, compared to the other data. 
+ExcelData=read_excel('ExcelFinalSheet.xlsx')
+
+#Before we make linear models, we need to explain that meth and coverage are subsets under the "factor" category
+meth<-subset(ExcelData,ExcelData$factor=="meth")
+coverage<-subset(ExcelData, ExcelData$factor=="coverage")
+
+lmmethchange<-lm(ExcelData$meth~ExcelData$%change)
+
+?lm
+
+
+
 setwd("C:/GitHub/ritcheyms")
